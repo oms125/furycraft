c2dd9b38e978885587432e125b701a4f7e769381
oms125 <silsbeeowen@gmail.com>
2025-12-22 22:01:05 -0500

    init

---
+++ src/client/java/com/furycraft/FuryCraftClient.java
@@ -0,0 +1,10 @@
+package com.furycraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FuryCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
